2c26ba9ddc89022106058ee805d3673b7ccb53c9
johnwhitton <john@johnwhitton.com>
2025-07-12 23:06:49 -0700

    Clean up IUniswapV4IntentRouter imports

--- src/interfaces/router/IUniswapV4IntentRouter.sol
+++ src/interfaces/router/IUniswapV4IntentRouter.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
-import { PathKey } from "../libraries/PathKey.sol";
+import { PathKey } from "./PathKey.sol";
 import { PoolKey } from "@uniswap/v4-core/src/types/PoolKey.sol";
 import { Currency } from "@uniswap/v4-core/src/types/Currency.sol";
 import { BalanceDelta } from "@uniswap/v4-core/src/types/BalanceDelta.sol";
-import { ISignatureTransfer } from "@permit2/interfaces/ISignatureTransfer.sol";
 
 /// @title Uniswap V4 Swap Router
 /// @notice A simple, stateless router for execution of swaps against Uniswap v4 Pools
